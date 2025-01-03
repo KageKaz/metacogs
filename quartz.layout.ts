93f9b2a5b9cc1215767528ecc0277492600a5cf5
Daniel <dshu.mcs@gmail.com>
2025-01-03 11:56:46 -0800

    Quartz sync: Jan 3, 2025, 11:56 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,6 +1,7 @@
 import { title } from "node:process"
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
+import { SimpleSlug } from "./quartz/util/path"
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
@@ -27,22 +28,34 @@ export const defaultContentPageLayout: PageLayout = {
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
-    Component.DesktopOnly(Component.Explorer(
-      {title: "pages",
-       folderClickBehavior: "link",
-       folderDefaultState: "open",
+    Component.DesktopOnly(Component.RecentNotes(
+      {
+        title: "recent",
+        linkToMore: "cogs/" as SimpleSlug,
       }
     )),
   ],
   right: [
-    Component.DesktopOnly(Component.TableOfContents()),
-    Component.MobileOnly(Component.Explorer(
-      {title: "pages",
-       folderClickBehavior: "link",
-       folderDefaultState: "open",
+    /*
+    Component.DesktopOnly(Component.Explorer(
+      {
+        title: "tags",
+        filterFn: (node) => {
+          // set containing names of everything you want to filter out
+          const omit = new Set(["cogs"])
+          return !omit.has(node.name.toLowerCase())
+        },
       }
     )),
-    Component.Backlinks(),
+    */
+    Component.DesktopOnly(Component.TableOfContents()),
+    Component.MobileOnly(Component.RecentNotes(
+      {
+        title: "recent",
+        linkToMore: "cogs/" as SimpleSlug,
+      }
+    )),
+    Component.DesktopOnly(Component.Backlinks()),
   ],
 }
 
@@ -53,18 +66,19 @@ export const defaultListPageLayout: PageLayout = {
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
-    Component.DesktopOnly(Component.Explorer(
-      {title: "pages",
-       folderClickBehavior: "link",
-       folderDefaultState: "open",
+    Component.DesktopOnly(Component.RecentNotes(
+      {
+        title: "recent",
+        linkToMore: "cogs/" as SimpleSlug,
+
       }
     )),
   ],
   right: [
-    Component.MobileOnly(Component.Explorer(
-      {title: "pages",
-       folderClickBehavior: "link",
-       folderDefaultState: "open",
+    Component.MobileOnly(Component.RecentNotes(
+      {
+        title: "recent",
+        linkToMore: "cogs/" as SimpleSlug,
       }
     )),
   ],
