611e31476f3c4c3eb4ba91deb2bf7c06537084ef
Daniel <dshu.mcs@gmail.com>
2025-01-03 17:29:00 -0800

    Quartz sync: Jan 3, 2025, 5:28 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -19,7 +19,7 @@ const config: QuartzConfig = {
     baseUrl: "metacogs.pages.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
-    generateSocialImages: false,
+    generateSocialImages: true,
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
